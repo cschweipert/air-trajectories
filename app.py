62caa2e8ea086e7b94fc7961bc92d21571401497
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-04-13 17:39:16 -0600

    refactor

--- app.py
+++ app.py
@@ -5,30 +5,32 @@ import dash_leaflet as dl
 
 app = Dash(__name__)
 
-r = requests.get(f'https://api.energyandcleanair.org/v1/trajectories?location_id=kuala lumpur_mys.4_1_my&date=2022-01-05')
+def create_layer():
+    r = requests.get(f'https://api.energyandcleanair.org/v1/trajectories?location_id=kuala lumpur_mys.4_1_my&date=2022-01-05')
 
-data = r.json().get('data')
+    data = r.json().get('data')
 
-trajectories = data[0].get('features')
+    trajectories = data[0].get('features')
 
-polylines = []
+    polylines = []
 
-for trajectory in trajectories:
-    coordinates = trajectory.get('geometry').get('coordinates')
+    for trajectory in trajectories:
+        coordinates = trajectory.get('geometry').get('coordinates')
 
-    polyline = [[location[1], location[0]] for location in coordinates]
-    polylines.append(polyline)
+        polyline = [[location[1], location[0]] for location in coordinates]
+        polylines.append(polyline)
 
-dash_polylines = [dl.Polyline(positions=poly, color='blue') for poly in polylines]
+    dash_polylines = [dl.Polyline(positions=poly, color='blue') for poly in polylines]
+    map_center = [polylines[0][0][0], polylines[0][0][1]]
 
-feature_group = dl.FeatureGroup(
-    children=dash_polylines
-)
+    return [dl.FeatureGroup(children=dash_polylines), map_center]
+
+
+feature_group, center = create_layer()
 
 app.layout = html.Div([  
     dl.Map([dl.TileLayer(), feature_group], 
-           center=(polylines[0][0][0], 
-           polylines[0][0][1]), 
+           center=center, 
            zoom=6, 
            style={'height': '50vh'}),  
 ])
